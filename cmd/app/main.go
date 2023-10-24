e7e31b80598eff63678b171e568cf892cbea55e4
trvrenglish <trvrenglish@gmail.com>
2023-10-23 17:44:03 -0700

    initial connection

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"database/sql"
-	"github.com/mattn/go-sqlite3"
+	"fmt"
 	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "FPA_FOD_20221014.sqlite")
+	db, err := sql.Open("sqlite3", "../FPA_FOD_20221014.sqlite")
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Print(db)
+
 	defer db.Close()
 
 }
