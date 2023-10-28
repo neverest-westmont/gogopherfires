d13e1da7f032b77def918aa4780d999cd48e46ff
Nancy Everest <neverest@us.ci.org>
2023-10-28 08:29:10 -0700

    adding select and display data

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	db, err := sql.Open("sqlite3", "../FPA_FOD_20221014.sqlite")
+	// database sqlite file should be located in the app folder
+	firedb, err := sql.Open("sqlite3", "../FPA_FOD_20221014.sqlite")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(db)
+	defer firedb.Close()
+	displayData(firedb)
+}
 
-	defer db.Close()
-
+func displayData(db *sql.DB) {
+	row, err := db.Query("SELECT NWCG_REPORTING_UNIT_NAME, FIRE_SIZE FROM Fires WHERE NWCG_REPORTING_UNIT_NAME = 'Eldorado National Forest' ORDER BY FIRE_SIZE")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var name string
+		var firesize string
+		row.Scan(&name, &firesize)
+		log.Println("Fire: ", name, " ", firesize)
+	}
 }
