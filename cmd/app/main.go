767af5f8bb9cbd10eb349b70228e53c45209b9e4
trvrenglish <trvrenglish@gmail.com>
2023-11-29 14:46:46 -0800

    both maps load, but concurrent one is not concurrent

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
 	_ "github.com/mattn/go-sqlite3"
@@ -21,10 +19,6 @@ type Fire struct {
 	Year      string
 }
 
-func getRoute(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "mapPage.html")
-}
-
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -33,8 +27,8 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func websocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+func websocketHandler(writer http.ResponseWriter, request *http.Request) {
+	conn, err := upgrader.Upgrade(writer, request, nil)
 	if err != nil {
 		log.Println(err)
 		return
@@ -42,82 +36,63 @@ func websocketHandler(w http.ResponseWriter, r *http.Request) {
 	defer conn.Close()
 
 	var wg sync.WaitGroup
-	wg.Add(1)
+	defer wg.Wait()
 
-	go callLinear(conn, &wg)
-	//go callConcurrent(conn, &wg)
-
-	wg.Wait()
+	if request.URL.Path == "/serial" {
+		wg.Add(1)
+		go sendToWebSocket(conn, "serialOut1", &wg)
+	} else if request.URL.Path == "/concurrent" {
+		wg.Add(1)
+		go sendToWebSocket(conn, "concurrentOut1", &wg)
+	}
 }
 
 func main() {
-	// http.HandleFunc("/", getRoute)
-	http.HandleFunc("/websocket", websocketHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "mapPage.html")
+	})
+	http.HandleFunc("/serial", websocketHandler)
+	http.HandleFunc("/concurrent", websocketHandler)
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	fmt.Println("Waiting for goroutines to finish...")
-	wg.Wait()
-	fmt.Println("Finished.")
 }
 
-func callLinear(conn *websocket.Conn, wg *sync.WaitGroup) {
+func sendToWebSocket(conn *websocket.Conn, outfile string, wg *sync.WaitGroup) {
 	defer wg.Done()
-	var wgl sync.WaitGroup
-	defer timer("callLinear")()
 
-	wgl.Add(1)
-	linearFires := displayData("linearOut1", &wgl)
-	linearFiresJSON, err := json.Marshal(linearFires)
+	fires, err := fetchFireData()
 	if err != nil {
-		log.Println("Error marshaling linearFires:", err)
+		log.Println("Error fetching data from database:", err)
 		return
 	}
 
-	err = conn.WriteMessage(websocket.TextMessage, linearFiresJSON)
+	firesJSON, err := json.Marshal(fires)
 	if err != nil {
-		log.Println("Error sending linearFires through WebSocket:", err)
+		log.Println("Error marshaling fires:", err)
+		return
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, firesJSON)
+	if err != nil {
+		log.Println("Error sending fires through WebSocket:", err)
 		return
 	}
 }
 
-func callConcurrent(conn *websocket.Conn, wg *sync.WaitGroup) {
-	defer wg.Done()
-	var wgc sync.WaitGroup
-	defer timer("callConcurrent")()
-
-	wgc.Add(1)
-	concurrentFires := displayData("concurrentOut1", &wgc)
-	concurrentFiresJSON, err := json.Marshal(concurrentFires)
-	if err != nil {
-		log.Println("Error marshaling linearFires:", err)
-		return
-	}
-
-	err = conn.WriteMessage(websocket.TextMessage, concurrentFiresJSON)
-	if err != nil {
-		log.Println("Error sending linearFires through WebSocket:", err)
-		return
-	}
-}
-
-func displayData(outfile string, wg *sync.WaitGroup) []Fire {
-	defer wg.Done()
+func fetchFireData() ([]Fire, error) {
 	firedb, err := sql.Open("sqlite3", "../../internal/db/FPA_FOD_20221014.sqlite")
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer firedb.Close()
+
 	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
 							FROM Fires
-							WHERE NWCG_REPORTING_UNIT_NAME = 'Eldorado National Forest'
-							ORDER BY FIRE_SIZE ASC LIMIT 100`)
+							LIMIT 1000`)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer row.Close()
 
@@ -127,18 +102,8 @@ func displayData(outfile string, wg *sync.WaitGroup) []Fire {
 		var fire Fire
 		row.Scan(&fire.Name, &fire.FireSize, &fire.Latitude, &fire.Longitude, &fire.Year)
 		log.SetFlags(0)
-		fmt.Println(fire)
 		fires = append(fires, fire)
 	}
-	fmt.Println(fires)
 
-	return fires
-}
-
-func timer(name string) func() {
-	start := time.Now()
-	return func() {
-		duration := time.Since(start)
-		fmt.Printf("%s took %10f seconds\n", name, duration.Seconds())
-	}
+	return fires, nil
 }
