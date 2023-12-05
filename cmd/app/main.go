9ee4296816d88a7c8e7e9686ed57c694bbb0d706
Nancy Everest <neverest@us.ci.org>
2023-12-04 22:01:26 -0800

    order working

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -34,17 +34,16 @@ func main() {
 
 	fmt.Println("Waiting for goroutines to finish...")
 	wg.Wait()
-	fmt.Println("Finished.")
+	fmt.Println("Finished main.")
 
 }
 
 func callLinear(wg *sync.WaitGroup) {
 	defer wg.Done()
-	var wgl sync.WaitGroup
 	defer timer("callLinear")()
 
-	wgl.Add(3)
-	displayData("linearOut1", &wgl)
+	displayDataLinear("linearOut1")
+	fmt.Println("Finished callLinear.")
 }
 
 func callConcurrent(wg *sync.WaitGroup) {
@@ -52,11 +51,43 @@ func callConcurrent(wg *sync.WaitGroup) {
 	var wgc sync.WaitGroup
 	defer timer("callConcurrent")()
 
-	wgc.Add(3)
-	go displayData("concurrentOut1", &wgc)
+	wgc.Add(1)
+	go displayDataConcurrent("concurrentOut1", &wgc)
+	fmt.Println("Finished callConcurrent.")
 }
 
-func displayData(outfile string, wg *sync.WaitGroup) {
+func displayDataLinear(outfile string) {
+	file, err := os.Create(outfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	firedb, err := sql.Open("sqlite3", "../../internal/db/FPA_FOD_20221014.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer firedb.Close()
+	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
+							FROM Fires
+							WHERE NWCG_REPORTING_UNIT_NAME = 'Eldorado National Forest'
+							ORDER BY FIRE_SIZE ASC LIMIT 3000000`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer row.Close()
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var name string
+		var firesize string
+		var latitude string
+		var longitude string
+		var year string
+		row.Scan(&name, &firesize, &latitude, &longitude, &year)
+		log.SetFlags(0)
+		mw := io.MultiWriter(file)
+		fmt.Fprintf(mw, "Fire: %s %s %s %s %s\n", name, firesize, latitude, longitude, year)
+	}
+}
+
+func displayDataConcurrent(outfile string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	file, err := os.Create(outfile)
 	if err != nil {
@@ -70,7 +101,7 @@ func displayData(outfile string, wg *sync.WaitGroup) {
 	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
 							FROM Fires
 							WHERE NWCG_REPORTING_UNIT_NAME = 'Eldorado National Forest'
-							ORDER BY FIRE_SIZE ASC LIMIT 3`)
+							ORDER BY FIRE_SIZE ASC LIMIT 3000000`)
 	if err != nil {
 		log.Fatal(err)
 	}
