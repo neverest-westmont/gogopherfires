99f4833e2bba525297fa078105eb94f9df880382
Nancy Everest <neverest@us.ci.org>
2023-12-04 21:19:44 -0800

    reverted

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -22,10 +22,10 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", getRoute)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	// http.HandleFunc("/", getRoute)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	var wg sync.WaitGroup
 	wg.Add(2)
@@ -44,9 +44,7 @@ func callLinear(wg *sync.WaitGroup) {
 	defer timer("callLinear")()
 
 	wgl.Add(3)
-	displayData1("linearOut1", &wgl)
-	displayData2("linearOut2", &wgl)
-	displayData3("linearOut3", &wgl)
+	displayData("linearOut1", &wgl)
 }
 
 func callConcurrent(wg *sync.WaitGroup) {
@@ -55,12 +53,10 @@ func callConcurrent(wg *sync.WaitGroup) {
 	defer timer("callConcurrent")()
 
 	wgc.Add(3)
-	go displayData1("concurrentOut1", &wgc)
-	go displayData2("concurrentOut2", &wgc)
-	go displayData3("concurrentOut3", &wgc)
+	go displayData("concurrentOut1", &wgc)
 }
 
-func displayData1(outfile string, wg *sync.WaitGroup) {
+func displayData(outfile string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	file, err := os.Create(outfile)
 	if err != nil {
@@ -74,72 +70,7 @@ func displayData1(outfile string, wg *sync.WaitGroup) {
 	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
 							FROM Fires
 							WHERE NWCG_REPORTING_UNIT_NAME = 'Eldorado National Forest'
-							AND FIRE_SIZE > 22
-							ORDER BY FIRE_SIZE ASC`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer row.Close()
-	for row.Next() { // Iterate and fetch the records from result cursor
-		var name string
-		var firesize string
-		var latitude string
-		var longitude string
-		var year string
-		row.Scan(&name, &firesize, &latitude, &longitude, &year)
-		log.SetFlags(0)
-		mw := io.MultiWriter(file)
-		fmt.Fprintf(mw, "Fire: %s %s %s %s %s\n", name, firesize, latitude, longitude, year)
-	}
-}
-
-func displayData2(outfile string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	file, err := os.Create(outfile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	firedb, err := sql.Open("sqlite3", "../../internal/db/FPA_FOD_20221014.sqlite")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer firedb.Close()
-	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
-							FROM Fires
-							WHERE FIRE_SIZE < 22
-							ORDER BY FIRE_SIZE ASC`)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer row.Close()
-	for row.Next() { // Iterate and fetch the records from result cursor
-		var name string
-		var firesize string
-		var latitude string
-		var longitude string
-		var year string
-		row.Scan(&name, &firesize, &latitude, &longitude, &year)
-		log.SetFlags(0)
-		mw := io.MultiWriter(file)
-		fmt.Fprintf(mw, "Fire: %s %s %s %s %s\n", name, firesize, latitude, longitude, year)
-	}
-}
-
-func displayData3(outfile string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	file, err := os.Create(outfile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	firedb, err := sql.Open("sqlite3", "../../internal/db/FPA_FOD_20221014.sqlite")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer firedb.Close()
-	row, err := firedb.Query(`SELECT FIRE_NAME, FIRE_SIZE, LATITUDE, LONGITUDE, FIRE_YEAR
-								FROM Fires
-								WHERE FIRE_SIZE > 22
-								ORDER BY FIRE_SIZE ASC`)
+							ORDER BY FIRE_SIZE ASC LIMIT 3`)
 	if err != nil {
 		log.Fatal(err)
 	}
